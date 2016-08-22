2fac0488b30dbf9e23a2087e76e96f2d7e14b8a6
ARomeroDSCTEI <aromerocyt@gmail.com>
2016-08-22 13:15:21 -0430

    Se optimizo el código, menos lineas de código, facil de comprender.

--- search_ip.sh
+++ search_ip.sh
@@ -6,55 +6,35 @@
 echo " [(+.+)] Iniciando Escaner"
 echo 
 
-is_root=false
-
 if [ $( id -u ) != 0 ]; then
-	# Not ROTT
-	addr=$(ip addr | awk '/inet /{print substr($2,1)}' | tr " " "\n");
+	# Not ROOT
+	addr=$(ip addr | grep "dynamic" | awk '/inet /{print $4}' | tr "." "\n" );
 else
 	# Solo ROOT
-	is_root=true
-	addr=$(ifconfig | awk '/inet addr/{print substr($2,6)}' | tr " " "\n");
+	addr=$(ifconfig | awk '/inet addr/{print substr($2,6)}' | tr '\n' ' ' | tr "." "\n");
 fi
 
-for ip in $addr; do
-	if [ $is_root = false ]; then
-		e=0;
-		for t in $addr ; do
-			if [ $e = 2 ]; then
-				ip=$t;
-				break;
-			fi
-			e=$(( $e + 1 ));
-		done
-	fi
-
-	puntos_ip=$(echo $ip | tr "." "\n");
-
-	ip="";
-	i=0;
+ip="";
+i=0;
 	
-	for x in $puntos_ip; do
-		ip=$ip$x.
-		i=$(( $i + 1 ))
-		if [ $i -eq 3 ]; then
-			break;
-		fi
-	done
+for x in $addr; do
+	ip=$ip$x.
+	i=$(( $i + 1 ))
+	if [ $i -eq 3 ]; then
+		break;
+	fi
+done
 
-    i=1;
+i=1;
 
-    while [ 1 ]; do
-    	echo  [-] $ip$i;
-		wget -q -nv -t 1 --timeout=1 -S $ip$i -O /dev/null > /dev/null 2>&1
-		if [ $? -eq 0 ]; then
-			echo -e "\033[1;42m[+] Servidor Web: $ip$i \033[m";
-		fi
-		i=$(( $i + 1 ));
-		if [ $i -eq "256" ]; then
-			break;
-		fi
-	done
-
-	break;
+while [ 1 ]; do
+    echo  [-] $ip$i;
+	wget -q -nv -t 1 --timeout=1 -S $ip$i -O /dev/null > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo -e "\033[1;42m[+] Servidor Web: $ip$i \033[m";
+	fi
+	i=$(( $i + 1 ));
+	if [ $i -eq "256" ]; then
+		break;
+	fi
 done
